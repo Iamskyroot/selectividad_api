a6fdb9122f09d773d8f4f4441dd0d3e06c7630b4
Iamskyroot <90767482+Iamskyroot@users.noreply.github.com>
2024-10-23 13:52:56 +0100

    Uploading files

---
+++ src/main/java/com/skyroot/training_backend/controller/AuthController.java
@@ -0,0 +1,78 @@
+package com.skyroot.training_backend.controller;
+
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skyroot.training_backend.dto.LoginDto;
+import com.skyroot.training_backend.dto.UserDto;
+import com.skyroot.training_backend.model.Role;
+import com.skyroot.training_backend.model.User;
+import com.skyroot.training_backend.repository.RoleRepository;
+import com.skyroot.training_backend.repository.UserRepository;
+
+@RestController
+@RequestMapping("/training-api/auth")
+public class AuthController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/signin")
+    public ResponseEntity<String> authenticateUser(@RequestBody LoginDto loginDto){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginDto.getUsernameOrEmail(), loginDto.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new ResponseEntity<>("User signed-in successfully!.", HttpStatus.OK);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody UserDto userDto){
+
+        // add check for username exists in a DB
+        if(userRepository.existsByUsername(userDto.getUsername())){
+            return new ResponseEntity<>("Username is already taken!", HttpStatus.BAD_REQUEST);
+        }
+
+        // add check for email exists in DB
+        if(userRepository.existsByEmail(userDto.getEmail())){
+            return new ResponseEntity<>("Email is already taken!", HttpStatus.BAD_REQUEST);
+        }
+
+        // create user object
+        User user = new User();
+        user.setName(userDto.getName());
+        user.setUsername(userDto.getUsername());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+
+        Role roles = roleRepository.findByName("ROLE_ADMIN").get();
+        user.setRoles(Collections.singleton(roles));
+
+        userRepository.save(user);
+
+        return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
+
+    }
+
+}
---
+++ src/main/java/com/skyroot/training_backend/dto/UserDto.java
@@ -0,0 +1,12 @@
+package com.skyroot.training_backend.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+
+}
